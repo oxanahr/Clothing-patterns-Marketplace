4f52910b1f6896b16df2729b0be9b7d1f62a4e4f
oxanahr <oxana.hristova@gmail.com>
2018-09-16 17:29:40 +0300

    Create pom.xml with used dependencies

---
+++ src/main/java/com/web/patterns_marketplace/PatternsMarketplaceApplication.java
@@ -0,0 +1,16 @@
+package com.web.patterns_marketplace;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+@SpringBootApplication
+
+public class PatternsMarketplaceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PatternsMarketplaceApplication.class, args);
+
+    }
+}
