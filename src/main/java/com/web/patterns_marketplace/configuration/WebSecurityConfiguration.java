8e5e8f0da9be9a9a7af7ca983254fa0037a06722
oxanahr <oxana.hristova@gmail.com>
2018-09-19 13:06:19 +0300

    Add WebSecurityConfiguration.java and UserRegisterDto

---
+++ src/main/java/com/web/patterns_marketplace/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.web.patterns_marketplace.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers().permitAll();
+    }
+}
